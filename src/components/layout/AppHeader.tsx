29595d7bd6d262c36b9ddde2b16396ebffbf914b
Matheus Yoshiura <matheusyoshiura17@gmail.com>
2025-06-01 16:26:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div role="menuitem" className="relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0" tabIndex=-1 data-orientation="vertical" data-radix-collection-item="">
    
    with the text content:
    
        Suporte
    
    Relevant files:
    
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_dcc540d3._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_dcc540d3._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_dcc540d3._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/src_5aa2b27b._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748782464955.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    tire esse botão por favor

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -131,7 +131,7 @@ export default function AppHeader() {
                  <UserCircle className="mr-2 h-4 w-4" /> Perfil
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>Suporte</DropdownMenuItem>
+            
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={handleLogout} className="text-destructive focus:text-destructive-foreground focus:bg-destructive">
               Sair
